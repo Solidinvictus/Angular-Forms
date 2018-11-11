b022de670de37245d07ac9ab6b4949bebd92fc51
Ali-PC\Ali <alismili27@gmail.com>
2018-11-11 20:27:44 +0100

    [Add] Adding project to repository - First commit

---
+++ Angular Forms/tdforms/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService} from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React','Vue'];
+  topicHasError = true;
+  userModel = new User('Ali', 'alismili@info', 678880510,'default','morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService){}
+  validateTopic(value){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    // console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success!', data),
+      error=> console.error('Error!', error)
+    )
+  }
+}
